6e572225e86bffefb1b446aa711733fb655e284a
Marko Sarafijanovic <118335343+markosaraf@users.noreply.github.com>
2026-09-03 13:03:55 +0200

    Add app_j_ads_compatibility_useQAdManager module

---
+++ scripts/gravity_research/modules2024/app_j_ads_compatibility_useQAdManager.ts
@@ -0,0 +1,5 @@
+// MODULE: ./app/j/ads/compatibility/useQAdManager.ts
+// pos: 15979
+(function (module, exports, require) {
+e,t,s)=>{"use strict";s.d(t,{Z:()=>o});s("./node_modules/core-js/modules/es.array.iterator.js"),s("./node_modules/core-js/modules/web.dom-collections.iterator.js");var n=s("./node_modules/react/index.js");const o=()=>{const[e,t]=(0,n.useState)(),o=(0,n.useCallback)((()=>(0,s("./app/j/ads/promises.ts").L)().then((e=>{t(e)}))),[]);return(0,n.useEffect)((()=>{o()}),[o]),e}}
+});
